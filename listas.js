da7c2df41d77d13114236cef23de572260e23a8b
GAMELEIRA <gameleira270499@gmail.com>
2020-05-07 03:22:24 -0300

    Criado o arquivo listas.js que têm como objetivo trabalhar com listas

---
+++ listas.js
@@ -0,0 +1,19 @@
+console.log('Trabalhando com listas');
+
+const salvador = 'Salvador';
+
+const saoPaulo = 'São Paulo';
+
+const lista_destinos = new Array(
+    'Salvador',
+    'São Paulo',
+    'Rio de Janeiro',
+);
+
+lista_destinos.push('Curitiba');
+
+console.log('Destinos possiveis');
+
+console.log(salvador, saoPaulo);
+
+console.log(lista_destinos);
