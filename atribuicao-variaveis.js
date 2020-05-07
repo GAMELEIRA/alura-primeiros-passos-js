4876e1d77316f7a7ac2753c3396ffc558e5e6ff6
GAMELEIRA <gameleira270499@gmail.com>
2020-05-07 03:21:18 -0300

    Criado o arquivo atribuicao-variaveis.js que têm como objetivo trabalhar as atibuições que podemm ser feitas com o decorrer da interpretação do cod

---
+++ atribuicao-variaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribuicao de variaveis");
+
+const idade = 29;
+
+let primeiro_nome = "Gabriel";
+
+const sobrenome = " Gameleira";
+
+console.log(primeiro_nome + sobrenome);
+
+console.log(`Meu nome é %${primeiro_nome}`);
+
+const nomeCompleto = primeiro_nome + sobrenome;
+
+console.log(nome);
