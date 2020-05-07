378f0314ea7b002c09c5e5c859811f3bbbc10276
GAMELEIRA <gameleira270499@gmail.com>
2020-05-07 03:17:15 -0300

    Criado o arquivo conversao-tipos.js que tẽm como objetivo trabalhar com parses para conversão de tipos strings para númericos.

---
+++ conversao-tipos.js
@@ -0,0 +1,17 @@
+console.log("Conversão de tipos");
+
+console.log(" ano " + 2020);
+
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("10" / "2");
+
+const a = 1223;
+
+console.log(a.toString());
+
+console.log(typeof a.toString());
+
+console.log(6.5);
+
+console.log(7, 3);
